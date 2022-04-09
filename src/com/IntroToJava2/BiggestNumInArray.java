78adff8d600f3261fa27c1f74acf04043ac8a8b9
martin.vasilev <martin.vasilev@mentormate.com>
2022-04-09 18:16:18 +0300

    Adding one exercise

---
+++ src/com/IntroToJava2/BiggestNumInArray.java
@@ -0,0 +1,21 @@
+package com.IntroToJava2;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class BiggestNumInArray {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter a number sequence delimeted by interval: ");
+        int[] numberSequence = Arrays.stream(scanner.nextLine().split(" "))
+                .mapToInt(e -> Integer.parseInt(e)).toArray();
+
+        Arrays.sort(numberSequence);
+        System.out.println(numberSequence[numberSequence.length-1]);
+
+
+
+
+    }
+}
